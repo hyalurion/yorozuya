21ea1b4a7a8ff2ec9f892291ceba5b7184ba1dff
静時 Chronie <188880511+chronie-shizutoki@users.noreply.github.com>
2026-05-23 23:29:47 +0800

    chore: 更新 Android Gradle 插件版本至 9.2.1

--- flutter_app/android/settings.gradle.kts
+++ flutter_app/android/settings.gradle.kts
@@ -17,12 +17,9 @@ pluginManagement {
     }
 }
 
-// flutter 与 AGP 9 不兼容，需要使用 8.13.0 版本，限制Gradle版本为8.14.1
-// https://docs.flutter.dev/release/breaking-changes/migrate-to-agp-9
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
 }
 
